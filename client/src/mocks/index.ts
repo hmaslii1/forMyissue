df87204769c0cf03f45982a6b3460f5ee47e1875
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-09-30 15:08:10 +0000

    [Studio] Add screen "HomeIconClassic"

--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,4 +1,5 @@
 export { mock_confirmation as confirmation } from './confirmation/index';
+export { mock_homeiconclassic as homeiconclassic } from './homeiconclassic/index';
 export { mock_login as login } from './login/index';
 export { mock_menu as menu } from './menu/index';
 export { mock_settings as settings } from './settings/index';
